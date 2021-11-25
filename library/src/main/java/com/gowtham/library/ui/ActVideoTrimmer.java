6410380366f658aa887106f60e4d65214650c3e7
ionutteslovan <45990851+ionutteslovan@users.noreply.github.com>
2021-11-25 13:39:25 +0200

    Replace getRealPath with getPath

--- library/src/main/java/com/gowtham/library/ui/ActVideoTrimmer.java
+++ library/src/main/java/com/gowtham/library/ui/ActVideoTrimmer.java
@@ -213,8 +213,8 @@ public class ActVideoTrimmer extends LocalizationActivity {
         try {
             Runnable fileUriRunnable = () -> {
                 uri = Uri.parse(bundle.getString(TrimVideo.TRIM_VIDEO_URI));
-//              String path = FileUtils.getPath(ActVideoTrimmer.this, uri);
-                String path=FileUtils.getRealPath(ActVideoTrimmer.this,uri);
+                String path = FileUtils.getPath(ActVideoTrimmer.this, uri);
+//                 String path=FileUtils.getRealPath(ActVideoTrimmer.this,uri);
                 uri = Uri.parse(path);
                 runOnUiThread(() -> {
                     LogMessage.v("VideoUri:: " + uri);
